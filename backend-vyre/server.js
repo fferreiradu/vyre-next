5939afc9564a2c1c24cc283bfce5c0ea6fd0174f
Eduarda Ferreira <eduarda08ferreira11@gmail.com>
2026-05-06 21:32:46 -0300

    update railway api

--- backend-vyre/server.js
+++ backend-vyre/server.js
@@ -4,7 +4,11 @@ import nodemailer from "nodemailer";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://vyre-next.vercel.app",
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 app.use(express.json());
 
 const transporter = nodemailer.createTransport({
