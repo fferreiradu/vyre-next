95d526926edb1ed93b7c8e28f6d06cffbf65c1c8
Eduarda Ferreira <eduarda08ferreira11@gmail.com>
2026-05-23 16:16:26 -0300

    update config email

--- api/send-email.js
+++ api/send-email.js
@@ -15,9 +15,7 @@ module.exports = async function handler(req, res) {
     }
 
     const transporter = nodemailer.createTransport({
-      host: 'mail.vyrenext.com',
-      port: 465,
-      secure: true,
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
